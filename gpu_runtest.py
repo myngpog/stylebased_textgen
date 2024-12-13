4f90eb4f5f0a14110712901f455f534da7df8eb3
myngbruh <pingpengpongpog@gmail.com>
2024-12-13 03:36:56 +0000

    fixed gitignore to include gpu_runtest

--- gpu_runtest.py
+++
@@ -1,6 +0,0 @@
-import torch
-
-"""To see if GPU is connected."""
-print(f"Is CUDA available: {torch.cuda.is_available()}")
-print(f"CUDA version: {torch.version.cuda}")
-print(f"Device: {torch.cuda.get_device_name(0) if torch.cuda.is_available() else 'No GPU detected'}")
